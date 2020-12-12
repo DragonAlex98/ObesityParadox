f3ad8627b07c60ad4968ceb14571c1dcf0187fac
Marco <marco.scarpetta@studenti.unicam.it>
2020-12-12 18:39:24 +0100

    Singleton for AdipocyteList and RCCList, Cell spawner created, TODO: TCell spawn

---
+++ src/utils/CellSpawner.java
@@ -0,0 +1,16 @@
+package utils;
+
+
+/**
+ * Class capable of spawning cells of all the types
+ * @author Marco
+ *
+ */
+public class CellSpawner {
+
+	// to spawn TCells once the dendritic cell has reached the lymph node
+	public static void tCellSpawner() {
+		//TODO
+	}
+
+}
