1852578bbadea6168b72ddf93bae31f9dbc6d5d9
DragonAlex98 <alessandro.antinori@studenti.unicam.it>
2020-12-13 19:04:52 +0100

    Creazione griglia con alcune cellule e adipociti nelle celle rimanenti
    Tre modalità di visualizzazione degli stili:
    -tramite file XML, come nel caso di MastCell (usa
    GameOfLifeProva.Dead.style_0.xml, anche se viene usato per le MastCell)
    -tramite apposita classe Style, semplice cerchio, come nel caso di
    CD8Style
    -tramite apposita classe Style, con immagine (nella cartella icons),
    come nel caso di AdipocyteStyle
    
    A mio parere la terza modalità è la più comoda, anche per quanto
    riguarda la visualizzazione
    
    In tutti e tre i casi, bisogna comunque aggiornare il file
    repast.simphony.action.display_1.xml di conseguenza

--- src/GameOfLifeProva/ContextCreator.java
+++ src/GameOfLifeProva/ContextCreator.java
@@ -2,6 +2,10 @@ package GameOfLifeProva;
 
 import java.util.Random;
 
+import cell.Cell;
+import cell.immune.CD8;
+import cell.immune.MastCell;
+import cell.notImmune.Adipocyte;
 import repast.simphony.context.Context;
 import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
@@ -13,54 +17,85 @@ import repast.simphony.space.grid.GridBuilderParameters;
 import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.space.grid.StickyBorders;
 
-public class ContextCreator implements ContextBuilder<Object> {
+public class ContextCreator implements ContextBuilder<Cell> {
 
 	@Override
-	public Context build(Context<Object> context) {
+	public Context<Cell> build(Context<Cell> context) {
 		context.setId("GameOfLifeProva");
 
 		Parameters params = RunEnvironment.getInstance().getParameters();
 		
 		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
-		Grid<Object> grid = gridFactory.createGrid("grid", context, new GridBuilderParameters<Object>(new StickyBorders(), new SimpleGridAdder<Object>(), false, params.getInteger("width"), params.getInteger("height")));
+		Grid<Cell> grid = gridFactory.createGrid("grid", context, new GridBuilderParameters<Cell>(new StickyBorders(), new SimpleGridAdder<Cell>(), false, params.getInteger("width"), params.getInteger("height")));
+		
+		int size = grid.getDimensions().getWidth() * grid.getDimensions().getHeight();
 
 		System.out.println("Altezza: " + grid.getDimensions().getHeight() + ", Larghezza: " + grid.getDimensions().getWidth());
 		
-		float percentage = params.getFloat("percentage");
-		System.out.println("Percentuale cellule vive: " + percentage*100 + "%");
+		float cd8Percentage = params.getFloat("cd8Percentage");
+		System.out.println("Percentuale cellule CD8: " + cd8Percentage*100 + "%");
+		int cd8CellsToCreate = (int)(size * cd8Percentage);
+		System.out.println("Numero cellule CD8 da creare: " + cd8CellsToCreate);
 		
-		int liveCellsToCreate = (int)((grid.getDimensions().getWidth() * grid.getDimensions().getHeight()) * percentage);
-		System.out.println("Numero cellule vive da creare: " + liveCellsToCreate);
+		float mastPercentage = params.getFloat("mastPercentage");
+		System.out.println("Percentuale cellule Mast: " + mastPercentage*100 + "%");
+		int mastCellsToCreate = (int)(size * mastPercentage);
+		System.out.println("Numero cellule Mast da creare: " + mastCellsToCreate);
+		
+		int left = size - cd8CellsToCreate - mastCellsToCreate;
+		
+		if (left < 0) {
+			System.out.println("Numero di cellule da creare maggiore del numero di celle della griglia!");
+			return null;
+		}
+		
+		System.out.println("Numero cellule Adipocyte da creare: " + left);
 		
 		int seed = params.getInteger("randomSeed");
 		
-		for (int i = 0; i < liveCellsToCreate; i++) {
-			Living live = new Living(grid);
-			context.add(live);
+		for (int i = 0; i < cd8CellsToCreate; i++) {
+			CD8 cd8 = new CD8(10, grid, 1.0);
+			context.add(cd8);
 			Random r = new Random(seed);
 			int row;
 			int column;
 			do {
 				row = r.nextInt(grid.getDimensions().getHeight());
 				column = r.nextInt(grid.getDimensions().getWidth());
-			} while (!grid.moveTo(live, column, row));
-			context.add(live);
+			} while (!grid.moveTo(cd8, column, row));
+			context.add(cd8);
 		}
 		
-		int deadCellCount = 0;
-		for (int r = 0; r < grid.getDimensions().getHeight(); r++) {
-			for (int c = 0; c < grid.getDimensions().getWidth(); c++) {
-				Dead dead = new Dead(grid);
-				context.add(dead);
-				if (grid.moveTo(dead, c, r)) {
-					context.add(dead);
-					deadCellCount++;
-				} else {
-					context.remove(dead);
+		for (int i = 0; i < mastCellsToCreate; i++) {
+			MastCell mast = new MastCell(10, grid);
+			context.add(mast);
+			Random r = new Random(seed);
+			int row;
+			int column;
+			do {
+				row = r.nextInt(grid.getDimensions().getHeight());
+				column = r.nextInt(grid.getDimensions().getWidth());
+			} while (!grid.moveTo(mast, column, row));
+			context.add(mast);
+		}
+		
+		int adipocyteCellsToCreate = 0;
+		if (left > 0) {
+			for (int r = 0; r < grid.getDimensions().getHeight(); r++) {
+				for (int c = 0; c < grid.getDimensions().getWidth(); c++) {
+					Adipocyte adipocyte = new Adipocyte(10, grid);
+					context.add(adipocyte);
+					if (grid.moveTo(adipocyte, c, r)) {
+						context.add(adipocyte);
+						adipocyteCellsToCreate++;
+					} else {
+						context.remove(adipocyte);
+					}
 				}
 			}
-		}
-		System.out.println("Numero cellule morte da creare: " + deadCellCount);
+		}		
+		
+		System.out.println("Numero cellule Adipocyte create: " + adipocyteCellsToCreate);
 
 		return context;
 	}
